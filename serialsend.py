0f0442de71e9ee07fd59784563a0813fa8c1077d
Ben Gelb <beg5670@gmail.com>
2012-04-22 01:25:38 -0400

    fixed problem with live stream

--- serialsend.py
+++ serialsend.py
@@ -1,20 +1,9 @@
-import sys, serial, time
+import serial
+import time
 
-port='/dev/tty.usbserial-A1001Neq'
-
-def main(val=5):
-    s = serial.SERIAL(port)
-    
-    time.sleep(1)
-    
-    w = s.write(val)
-    s.close()
-    print 'byes written to port:', w
-    print 'value written to port:', val
-
-if __name__ == '__main__':
-    args = sys.argv
-    try:
-        main(args[1])
-    except IndexError:
-        main()
+s = serial.Serial('COM3',9600)
+for i in range(0,5):
+	s.write('Y')
+	time.sleep(1)
+	s.write('N')
+	print s.readline()
